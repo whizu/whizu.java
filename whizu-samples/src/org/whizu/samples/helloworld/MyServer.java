7c6a42cc5de22043af9bc52bdb0fc6f6a05a71be
Rudy D'hauwe <rdhauwe@whizu.org>
2013-03-11 22:15:44 +0100

    Initial revision.

---
+++ whizu-samples/src/org/whizu/samples/helloworld/MyServer.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Rudy D'hauwe (Whizu)
+ * Licensed under the EUPL V.1.1
+ *   
+ * This Software is provided to You under the terms of the European 
+ * Union Public License (the “EUPL”) version 1.1 as published by the 
+ * European Union. Any use of this Software, other than as authorized 
+ * under this License is strictly prohibited (to the extent such use 
+ * is covered by a right of the copyright holder of this Software). 
+ *
+ * This Software is provided under the License on an “AS IS” basis and 
+ * without warranties of any kind concerning the Software, including 
+ * without limitation merchantability, fitness for a particular purpose, 
+ * absence of defects or errors, accuracy, and non-infringement of 
+ * intellectual property rights other than copyright. This disclaimer 
+ * of warranty is an essential part of the License and a condition for 
+ * the grant of any rights to this Software. 
+ *  
+ * For more  details, see <http://joinup.ec.europa.eu/software/page/eupl>.
+ *
+ * Contributors:
+ *     2013 - Rudy D'hauwe (Whizu) - initial API and implementation
+ *******************************************************************************/
+package org.whizu.samples.helloworld;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class MyServer {
+
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public void run() {
+		try {
+			Server server = new Server(8090);
+			WebAppContext context = new WebAppContext();
+			context.setDescriptor("C:/Java/workspace/example/web/WEB-INF/web.xml");
+			context.setResourceBase("C:/Java/workspace/example/web");
+			context.setContextPath("/");
+			context.setParentLoaderPriority(true);
+			server.setHandler(context);
+			server.start();
+			server.join();
+		} catch (Exception exc) {
+			throw new RuntimeException(exc);
+		}
+	}
+}
