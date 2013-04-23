f2b667a02f5be58aa59fbbcccdd6a016911b698f
Rudy D'hauwe <rdhauwe@whizu.org>
2013-04-24 00:40:55 +0200

    Add support for javassist dynamic proxying.

---
+++ whizu-server/src/main/java/org/whizu/annotations/ApplicationInvocationHandler.java
@@ -0,0 +1,21 @@
+package org.whizu.annotations;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class ApplicationInvocationHandler implements InvocationHandler {
+	private Object proxied;
+
+	public ApplicationInvocationHandler(Object proxied) {
+		this.proxied = proxied;
+	}
+
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		Method m = proxied.getClass().getMethod(method.getName(), method.getParameterTypes());
+		if (m.isAnnotationPresent(Css.class)) {
+			System.out.println("\tIn the annotation processor");
+		}
+		return method.invoke(proxied, args);
+	}
+}
---
+++ whizu-server/src/main/java/org/whizu/annotations/TestApplication.java
@@ -0,0 +1,25 @@
+package org.whizu.annotations;
+
+import org.whizu.ui.Application;
+import org.whizu.ui.UI;
+
+public class TestApplication implements Application {
+
+	@Override
+	@Css
+	public String getTitle() {
+		System.out.println("get title");
+		return "the-title";
+	}
+
+	@Override
+	@Css
+	public void init(UI ui) {
+		System.out.println("init");
+	}
+	
+	@Css
+	public void custom() {
+		System.out.println("custom");
+	}
+}
